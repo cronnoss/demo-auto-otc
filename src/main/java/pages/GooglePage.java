f112d4aba070b26c27de2af83177b48bbbbf7c2f
Evgeniy Karasev <karasevevg@gmail.com>
2021-06-11 15:49:43 +0300

    update sendKeys selector

--- src/main/java/pages/GooglePage.java
+++ src/main/java/pages/GooglePage.java
@@ -3,12 +3,9 @@ package pages;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
-import org.openqa.selenium.Keys;
-
 public class GooglePage {
 
     public void sendKeys(String text) {
-        $(byTagName("input")).sendKeys(text);
-        $(byTagName("input")).sendKeys(Keys.ENTER);
+        $(byName("q")).sendKeys(text + "\n");
     }
 }
